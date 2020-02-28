66bdb2f3ca0bbadb0e1171631fcf1e96e98c8471
brilli4ntc0der <cby141994@gmail.com>
2020-02-28 16:59:19 +0800

    instantaneous_randomized_response simplified

--- basic-RAPPOR.py
+++ basic-RAPPOR.py
@@ -8,16 +8,17 @@ Parameters
     d: binary vector length
 Returns
 -------
+    b: Unary Encoding vector
 """
 
 
 def encode(v, d):
-    b0 = []
+    b = []
     for i in range(d):
-        b0.append(0)
-    b0[v] = 1
+        b.append(0)
+    b[v] = 1
     #print(b0)
-    return b0
+    return b
 
 
 def permanent_randomized_response(b, d, f):
@@ -36,9 +37,7 @@ def permanent_randomized_response(b, d, f):
 def instantaneous_randomized_response(b, d, p, q):
     for i in range(d):
         r = random.uniform(0, 1)
-        if b[i] == 1 and r < p:
-            b[i] = 1
-        elif b[i] == 0 and r < q:
+        if b[i] == 1 and r <= p or b[i] == 0 and r <= q:
             b[i] = 1
         else:
             b[i] = 0
@@ -46,16 +45,17 @@ def instantaneous_randomized_response(b, d, p, q):
 
 
 def aggregation(b, d, f, n):
+    print(b)
     for i in range(d):
         b[i] = (b[i] - 0.5 * f * n) / (1 - f)
     return b
 
 
 def perturbation(b, d, f):
-    permanent_randomized_response(b, d, f)
-    instantaneous_randomized_response(b, d, 0.75, 0.25)
-    return b
-
+    b1 = permanent_randomized_response(b, d, f)
+    b2 = instantaneous_randomized_response(b1, d, 0.75, 0.25)
+    return b2
+    # return b1
 
 def test(d, f, n):
     b = []
@@ -86,4 +86,4 @@ def test1(d, f, n):
     for i in aggregation(b, d, f, n):
         print(i)
 
-test1(2, 0.5, 1000000)
+test(2, 0.5, 1000000)
