e94c2de25384a0a43625cf324c1e3adfb99bec36
brilli4ntc0der <cby141994@gmail.com>
2020-02-29 21:26:00 +0800

    annotation added for basic-RAPPOR

--- basic-RAPPOR.py
+++ basic-RAPPOR.py
@@ -1,27 +1,40 @@
 import random
-"""
-encode
 
-Parameters
-----------
-    v: value
-    d: binary vector length
-Returns
--------
-    b: Unary Encoding vector
-"""
 
 
 def encode(v, d):
+    """
+    encode `value` with the mechanism.
+
+    Parameters
+    ----------
+        v: value
+        d: binary vector length
+    Returns
+    -------
+        b: Unary Encoding vector
+    """
     b = []
     for i in range(d):
         b.append(0)
     b[v] = 1
-    #print(b0)
     return b
 
 
 def permanent_randomized_response(b, d, f):
+    """
+    replaces the real value B with a derived randomized noisy value B'
+
+    Parameters
+    ----------
+        b: real value vector
+        d: binary vector length
+        f:  a user-tunable parameter controlling the
+        level of longitudinal privacy guarantee
+    Returns
+    -------
+        b': randomized value vector
+    """
     for i in range(d):
         r = random.uniform(0, 1)
         if r * 2 < f:
@@ -35,27 +48,64 @@ def permanent_randomized_response(b, d, f):
 
 
 def instantaneous_randomized_response(b, d, p, q):
+    """
+    reports a randomized version of B'(B")
+    Parameters
+    ----------
+        b: randomized value vector
+        d: binary vector length
+        p: Pr [B2[i] = 1] = p if if B1[i] = 1
+        q: Pr [B2[i] = 1] = q if if B1[i] = 0
+    Returns
+    -------
+        b': randomized version of b
+    """
     for i in range(d):
         r = random.uniform(0, 1)
-        if b[i] == 1 and r <= p or b[i] == 0 and r <= q:
+        if b[i] == 1 and r < p or b[i] == 0 and r < q:
             b[i] = 1
         else:
+            # print(str(b[i])+' '+str(r))
             b[i] = 0
     return b
 
 
 def aggregation(b, d, f, n):
-    print(b)
+    """
+    aggregate the reported data
+    Parameters
+    ----------
+        b:  value vector after perturbation and accumulation
+        d: binary vector length
+        f:  a user-tunable parameter controlling the
+        level of longitudinal privacy guarantee
+        n: data size
+    Returns
+    -------
+        b: aggregate version of b"
+    """
     for i in range(d):
         b[i] = (b[i] - 0.5 * f * n) / (1 - f)
     return b
 
 
 def perturbation(b, d, f):
+    """
+    Parameters
+    ----------
+        b: real value vector
+        d: binary vector length
+        f:  a user-tunable parameter controlling the
+        level of longitudinal privacy guarantee
+    Returns
+    -------
+        b': value vector after perturbation
+    """
     b1 = permanent_randomized_response(b, d, f)
     b2 = instantaneous_randomized_response(b1, d, 0.75, 0.25)
     return b2
     # return b1
+
 
 def test(d, f, n):
     b = []
@@ -67,6 +117,7 @@ def test(d, f, n):
             b[j] = b[j] + b1[j]
     for i in aggregation(b, d, f, n):
         print(i)
+
 
 def test1(d, f, n):
     b = []
